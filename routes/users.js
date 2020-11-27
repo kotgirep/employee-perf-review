e2fa2f4f7e6bed864409f41fc614bfd565b9e792
kotgirep <pranjalikotgire57@gmail.com>
2020-11-26 20:24:41 -0800

    front end to rate employee

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  res.send('respond with a resource');
+});
+/* GET User rating here */
+
+router.get('/addRating', (req, res) => {
+  res.render('rating.html');
 });
 
 module.exports = router;
