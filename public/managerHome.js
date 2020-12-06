dd20c56a91a50460dfd8bd2c7ae73c7327711a57
kotgirep <pranjalikotgire57@gmail.com>
2020-12-05 23:58:20 -0800

    last modification

--- public/managerHome.js
+++ public/managerHome.js
@@ -1,93 +1,91 @@
-
 window.onload = function () {
-    var url = document.location.href,
-        params = url.split('?')[1].split('&'),
-        data = {}, tmp;
-    for (var i = 0, l = params.length; i < l; i++) {
-         tmp = params[i].split('=');
-         data[tmp[0]] = tmp[1];
-    }
-    listAllEmployees(data.deptID);
-}
+  var url = document.location.href,
+    params = url.split('?')[1].split('&'),
+    data = {},
+    tmp;
+  for (var i = 0, l = params.length; i < l; i++) {
+    tmp = params[i].split('=');
+    data[tmp[0]] = tmp[1];
+  }
+  listAllEmployees(data.deptID);
+};
 
+function listAllEmployees(deptNo, reset = true) {
+  console.log('inside priti');
+  const fetchURL = 'http://perfreview.ml/getEmployees';
 
-function listAllEmployees( deptNo, reset = true) {
-    console.log("inside priti");
-    const fetchURL = 'http://localhost:3000/getEmployees';
+  const deptID = {
+    dept_no: deptNo,
+  };
 
-const deptID = {
-      dept_no: deptNo,
-    };
-
-fetch(fetchURL, {
-      method: 'POST',
-      mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
-      body: JSON.stringify(deptID),
-      headers: {
-        'content-type': 'application/json',
-      },
-    })
-
-.then(response => response.json())
-    .then(result => {
+  fetch(fetchURL, {
+    method: 'POST',
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    body: JSON.stringify(deptID),
+    headers: {
+      'content-type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((result) => {
       console.log(result.statuses);
       console.log(result);
       slicedResult = result.slice(1, 100);
       //result.forEach(status => {
-      slicedResult.forEach(status => {
-		console.log(status);
-		var newRow = $("<tr>");
-                var cols = "";
-                cols += '<td> '+ status.first_name +'</td>';
-                cols += '<td> '+ status.last_name +'</td>';
-                cols += '<td> '+ status.emp_no +'</td>';
-                cols += '<td> '+ (status.hire_date).split('T')[0] +'</td>';
-                cols += '<td> '+ '<input type=\"button\" value=\"Rate Employee\" id=\'rate-button\' onClick=\'rateMe()\'/>' +'</td>';
-                cols += '<td> '+ '<input type=\"button\" value=\"Update Rating\" id=\'update-button\' onClick=\'updateRating()\'/>' +'</td>';
-                cols += '<td> '+ '<input type=\"button\" value=\"Show Rating\" id=\'show-button\' onClick=\'showRating(' + status.emp_no + ')\'/>' +'</td>';
-                newRow.append(cols);
-                $('#empData  .tbody').append(newRow);
-
-});
-});
+      slicedResult.forEach((status) => {
+        console.log(status);
+        var newRow = $('<tr>');
+        var cols = '';
+        cols += '<td> ' + status.first_name + '</td>';
+        cols += '<td> ' + status.last_name + '</td>';
+        cols += '<td> ' + status.emp_no + '</td>';
+        cols += '<td> ' + status.hire_date.split('T')[0] + '</td>';
+        cols +=
+          '<td> ' +
+          '<input type="button" value="Rate Employee" id=\'rate-button\' onClick=\'rateMe()\'/>' +
+          '</td>';
+        cols +=
+          '<td> ' +
+          '<input type="button" value="Update Rating" id=\'update-button\' onClick=\'updateRating()\'/>' +
+          '</td>';
+        cols +=
+          '<td> ' +
+          '<input type="button" value="Show Rating" id=\'show-button\' onClick=\'showRating(' +
+          status.emp_no +
+          ")'/>" +
+          '</td>';
+        newRow.append(cols);
+        $('#empData  .tbody').append(newRow);
+      });
+    });
 }
 
 function rateMe() {
-	url = 'http://localhost:3000/rate.html';
-	document.location.href = url;
+  url = 'http://perfreview.ml/rate.html';
+  document.location.href = url;
 }
 function showChart() {
   var url = document.location.href,
-        params = url.split('?')[1].split('&'),
-        data = {}, tmp;
-    for (var i = 0, l = params.length; i < l; i++) {
-         tmp = params[i].split('=');
-         data[tmp[0]] = tmp[1];
-    }
+    params = url.split('?')[1].split('&'),
+    data = {},
+    tmp;
+  for (var i = 0, l = params.length; i < l; i++) {
+    tmp = params[i].split('=');
+    data[tmp[0]] = tmp[1];
+  }
   //alert(data.empID);
-  url = 'http\://localhost:3000/employeeHome.html?empID=' + data.empID;
-	document.location.href = url;
+  url = 'http://perfreview.ml/employeeHome.html?empID=' + data.empID;
+  document.location.href = url;
 }
 
 function showRating(empID) {
-  
-  url = 'http\://localhost:3000/employeeHome.html?empID=' + empID;
-	document.location.href = url;
+  url = 'http://perfreview.ml/employeeHome.html?empID=' + empID;
+  document.location.href = url;
 }
 
 function updateRating() {
-	url = 'http://localhost:3000/updaterate.html';
-	document.location.href = url;
+  url = 'http://perfreview.ml/updaterate.html';
+  document.location.href = url;
 }
-
-
-
-
-
-
-
-
-
-
--- routes/index.js
+++ routes/index.js
@@ -3,43 +3,45 @@ var router = express.Router();
 var mySQL = require('mysql');
 
 var pool = mySQL.createPool({
-    connectionLimit: 1000,
-    host: "employee-database.c1ty2hg1gwnk.us-west-1.rds.amazonaws.com",
-    user: "admin",
-    password: "Techietribe"
+  connectionLimit: 1000,
+  host: 'employee-database.c1ty2hg1gwnk.us-west-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'Techietribe',
 });
-
-
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  console.log("entered get home page");
-  if(!req.userContext){
-    return res.render('login', { title: 'Express' })
+  console.log('entered get home page');
+  if (!req.userContext) {
+    return res.render('login', { title: 'Express' });
   }
 
-
-var employeeno = req.body.logged_emp_no;
+  var employeeno = req.body.logged_emp_no;
   pool.getConnection(function (err, connection) {
-      if (err) throw err;
-      connection.query("select * from `employees`.dept_manager where emp_no = " + employeeno,
-          function (err, result, fields) {
-            console.log(JSON.stringify(result));
-              if (err || result.length == 0) {
-                //res.render('employeeHome', { name: "example" });
-                console.log("placeholder");
-                console.log(result);
-                url = 'http\://localhost:3000/employeeHome.html?empID=' + employeeno;
-                res.redirect(url);
-              } else{
-                result =  JSON.parse(JSON.stringify(result));
-                //console.log(result[0].emp_no);
-                url = 'http\://localhost:3000/managerHome.html?deptID=' + result[0].dept_no + '&empID=' + employeeno;;
-                res.redirect(url);
-              }
-          });
-  })
-
+    if (err) throw err;
+    connection.query(
+      'select * from `employees`.dept_manager where emp_no = ' + employeeno,
+      function (err, result, fields) {
+        console.log(JSON.stringify(result));
+        if (err || result.length == 0) {
+          //res.render('employeeHome', { name: "example" });
+          console.log('placeholder');
+          console.log(result);
+          url = 'http://perfreview.ml/employeeHome.html?empID=' + employeeno;
+          res.redirect(url);
+        } else {
+          result = JSON.parse(JSON.stringify(result));
+          //console.log(result[0].emp_no);
+          url =
+            'http://perfreview.ml/managerHome.html?deptID=' +
+            result[0].dept_no +
+            '&empID=' +
+            employeeno;
+          res.redirect(url);
+        }
+      }
+    );
+  });
 });
 
 module.exports = router;
