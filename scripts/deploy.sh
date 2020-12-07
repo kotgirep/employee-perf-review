19dda756557a40708bda4e320f9d36c44da333aa
BhavyaTetali <bhavyatetali3@gmail.com>
2020-12-06 22:54:24 -0800

    fix script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ ssh ubuntu@52.71.116.115 <<EOF
     cd ~/employee-perf-review/
     git pull origin master
     npm install
-    npm install -g nodemon pm2
+    sudo npm install -g nodemon pm2
     pm2 restart ecosystem.config.js
     exit
 EOF
