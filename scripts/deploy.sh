f87292b3bad5e31fd4b5d6f8af439db4d9089ba8
BhavyaTetali <bhavyatetali3@gmail.com>
2020-12-06 22:22:39 -0800

    fix bug in script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 ssh ubuntu@52.71.116.115 <<EOF
     cd ~/employee-perf-review/
     git pull origin master
-    kill -9 $(lsof -t -i:3000)
+    lsof -t -i:3000 | xargs kill
     npm install
     nohup npm start &
 EOF
