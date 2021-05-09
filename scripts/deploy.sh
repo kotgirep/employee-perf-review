e81bd866d3861b03403b1612d85fb90ab12fa8ea
BhavyaTetali <bhavyatetali3@gmail.com>
2021-05-08 23:45:58 -0700

    add updated script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 ssh ubuntu@54.172.246.61 <<EOF
     cd ~/employee-perf-review/
-    git pull origin master
+    git pull 
     npm install
     sudo npm install -g nodemon pm2
     pm2 restart ecosystem.config.js
